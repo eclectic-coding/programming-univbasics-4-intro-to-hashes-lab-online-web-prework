e77356d953b48b8161aff4a7d6f2af68c4f11016
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-21 13:59:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,10 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  new_hash = {
+    :id => "Pete",
+    :age => 17
+  }
 end
 
 def my_hash_creator(key, value)
